8eb1baf1812f4afeeb3c2343441cc24102e91023
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-19 02:35:09 -0700

    🛠️ update quiz-back component

--- src/components/quiz/Back.jsx
+++ src/components/quiz/Back.jsx
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
+import { createTheme, ThemeProvider} from '@mui/material';
 
 export default function Back(props) {
-  const { currentCard, nextCard } = props;
+  const { currentCard, handleClick } = props;
+  const codiTheme = createTheme({});
 
   return (
-    <main className="quiz__card quiz__card--back">
-      <section className="quiz__card-text">
-        <h1>Back</h1>
-        <h4>{currentCard.front}</h4>
-        <h4>{currentCard.back}</h4>
-      </section>
-      <button className="quiz__card-again" onClick={nextCard}>
-        Again
+    <ThemeProvider theme={codiTheme}>
+      <main className="quiz__card quiz__card--back">
+        <section className="quiz__card-text">
+          <h1>Back</h1>
+          <h4>{currentCard.front}</h4>
+          <h4>{currentCard.back}</h4>
+        </section>
+      <button className="quiz__card-again" onClick={() => handleClick('AGAIN')} variant="contained" color="primary">
+      Again
       </button>
-      <button className="quiz__card-hard" onClick={nextCard}>
-        Hard
+      <button className="quiz__card-hard" onClick={() => handleClick('HARD')} variant="contained" color="primary">
+      Hard
       </button>
-      <button className="quiz__card-good" onClick={nextCard}>
-        Good
+      <button className="quiz__card-good" onClick={() => handleClick('GOOD')} variant="contained" color="primary">
+      Good
       </button>
-      <button className="quiz__card-easy" onClick={nextCard}>
-        Easy
+      <button className="quiz__card-easy" onClick={() => handleClick('EASY')} variant="contained" color="primary">
+      Easy
       </button>
-    </main>
-  );
+      </main>
+    </ThemeProvider>
+    );
 }
