b4fe63471b419a4b911d562dc5f31132e3e5049b
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-18 19:31:48 -0700

    ➕ add quiz component

--- src/components/quiz/index.jsx
+++ src/components/quiz/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
+import { getFlashcardsForQuiz } from "../../helpers/selector-quiz";
+import useApplicationData from "../../hooks/useApplicationData";
+import useVisualMode from "../../hooks/useVisualMode";
+import Front from "./Front";
+import Back from "./Back";
+import QuizComplete from "./QuizComplete";
+import { useState } from "react";
 
-const Quiz = () => {
+export default function Quiz(props) {
+  const [index, setIndex] = useState(0);
+  const { state } = useApplicationData();
+  const { mode, transition } = useVisualMode("FRONT");
+
+  const flashcardsToDisplay = getFlashcardsForQuiz(state);
+  const currentCard = flashcardsToDisplay[index];
+
+  const handleNext = () => {
+    setIndex((prev) => prev + 1);
+    if (index < flashcardsToDisplay.length - 1) {
+      transition('FRONT');
+    } else {
+      transition('COMPLETE');
+    }
+  };
+
   return (
-    <p>Quiz Page</p>
+    <>
+      {index < flashcardsToDisplay.length ? (
+        <>
+          {mode === 'FRONT' && 
+            <Front currentCard={currentCard} onClick={() => transition('BACK')}/>
+          }
+          {mode === 'BACK' && 
+            <Back currentCard={currentCard} nextCard={handleNext} />
+          }
+        </>
+      ) : (
+        <>
+        {mode === 'COMPLETE' && <QuizComplete />}
+        </>
+      )}
+    </>
   );
 }
-
-export default Quiz;
