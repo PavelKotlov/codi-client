f0993d1fab489b770da2d45cffcb46469f6c35bc
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-19 02:34:46 -0700

    🛠️ update quiz-front component

--- src/components/quiz/Front.jsx
+++ src/components/quiz/Front.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { createTheme, ThemeProvider} from '@mui/material';
 
 export default function Front(props) {
   const { currentCard, onClick } = props;
+  const codiTheme = createTheme({});
 
   return (
-    <main className="quiz__card quiz__card--front">
-      <section className="quiz__card-text">
-        <h1>Front</h1>
-        <h4>{currentCard.front}</h4>
-      </section>
-      <button
-        className="quiz__card-submit"
-        onClick={onClick} 
-      >Show Answer</button>
-    </main>
-  );
+    <ThemeProvider theme={codiTheme}>
+      <main className="quiz__card quiz__card--front">
+        <section className="quiz__card-text">
+          <h1>Front</h1>
+          <h4>{currentCard.front}</h4>
+        </section>
+        <button variant="contained" onClick={onClick}>
+        Show Answer
+        </button>
+      </main>
+    </ThemeProvider>
+    );
 }
 
  
