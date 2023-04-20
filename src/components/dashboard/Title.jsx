fe20645b3a243fe9c91c7c36d3218da608c13128
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-20 12:16:51 -0600

    add topic title component

---
+++ src/components/dashboard/Title.jsx
@@ -0,0 +1,24 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Title = ({ name }) => {
+  return (
+    <Typography
+      variant='h3'
+      component='h1'
+      sx={{
+        color: '#271D30',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+      }}
+      align='center'>
+      {name}
+    </Typography>
+  );
+};
+
+export default Title;
+
+{
+  /* <Typography variant='h3' component='h1' color='primary' align='center'> */
+}
