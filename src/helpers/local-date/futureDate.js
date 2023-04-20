c32ba9138ab617bb9b62c6ad10b6fc7bc1efb22e
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-20 12:15:04 -0600

    delete unused files

--- src/helpers/local-date/futureDate.js
+++
@@ -1,7 +0,0 @@
-const futureDate = function (days) {
-  const currentDate = new Date();
-  currentDate.setDate(currentDate.getDate() + days);
-  return currentDate;
-};
-
-module.exports = futureDate;
