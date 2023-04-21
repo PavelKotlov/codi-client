53d382ee30a952a31521f5c7329416560759e470
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:40:55 -0600

    menu component

---
+++ src/components/controllers/menu.jsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Avatar, ListItemIcon, Typography } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
+const ITEM_HEIGHT = 48;
+
+export default function NavMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        sx={{ position: 'absolute', top: 0, right: 0, marginRight: 3 }}
+        aria-label='more'
+        id='long-button'
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup='true'
+        onClick={handleClick}>
+        <MoreHorizIcon fontSize='large' />
+      </IconButton>
+      <Menu
+        id='long-menu'
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}>
+        <MenuItem onClick={handleClose}>
+          <Avatar />
+          <Typography variant='body2' color='text.secondary' p={1}>
+            Sara Miller
+          </Typography>
+        </MenuItem>
+        <MenuItem key='setting' onClick={handleClose}>
+          <ListItemIcon>
+            <SettingsIcon fontSize='small' />
+          </ListItemIcon>
+          <Typography variant='body2' color='text.secondary'>
+            Settings
+          </Typography>
+        </MenuItem>
+
+        <MenuItem key='logout' onClick={handleClose}>
+          <ListItemIcon>
+            <ExitToAppIcon fontSize='small' />
+          </ListItemIcon>
+          <Typography variant='body2' color='text.secondary'>
+            Logout
+          </Typography>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
