594d9f0e982d2c4a09744f16485386f96a15c4c2
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:42:12 -0600

    topics page

--- src/pages/topics.js
+++ src/pages/topics.js
@@ -1,7 +1,46 @@
-import React from "react";
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import TopicList from '../components/topics/TopicList';
+import Loading from '../components/Loading';
+import NavMenu from '../components/controllers/menu';
+import { Grid } from '@mui/material';
 
 const Topics = () => {
-  return <p>Topics Page</p>;
+  const [state, setState] = useState({
+    loading: true,
+    topics: [],
+  });
+
+  useEffect(() => {
+    Axios.get(`/api/topics`).then((res) => {
+      setState((prev) => ({
+        ...prev,
+        topics: res.data,
+        loading: false,
+      }));
+    });
+  }, []);
+
+  return (
+    <>
+      <NavMenu />
+      {state.loading ? (
+        <Loading />
+      ) : (
+        <Grid
+          sx={{
+            flowDirection: 'column',
+            maxHeight: '100vh',
+            px: '2%',
+            paddingTop: '8%',
+            width: '65%',
+            margin: '0 auto',
+          }}>
+          <TopicList topics={state.topics} />
+        </Grid>
+      )}
+    </>
+  );
 };
 
 export default Topics;
