0b81d6eb0080736ff99b3b16ecc25c966cffb23d
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:40:41 -0600

    close button component

---
+++ src/components/controllers/closeButton.jsx
@@ -0,0 +1,17 @@
+import { IconButton, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Close = ({ link }) => {
+  return (
+    <Link to={link} color='inherit'>
+      <IconButton
+        sx={{ position: 'absolute', left: 0, marginLeft: 3 }}
+        aria-label='close'>
+        <CloseIcon />
+      </IconButton>
+    </Link>
+  );
+};
+
+export default Close;
