3952add1da09dcdc72b3beeb35b777e9467519f4
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:42:27 -0600

    topics list component

---
+++ src/components/topics/TopicList.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import TopicListItem from './TopicListItem';
+import { Autocomplete, Grid, InputAdornment, TextField } from '@mui/material';
+import AddTopicButton from './AddTopicButton';
+import { Search } from '@mui/icons-material';
+
+const TopicList = ({ topics }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredTopics = topics.filter((topic) =>
+    topic.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const topicListItems = filteredTopics.map((topic) => {
+    return (
+      <TopicListItem
+        key={topic.id}
+        id={topic.id}
+        name={topic.name}
+        image_url={topic.image_url}
+      />
+    );
+  });
+
+  return (
+    <Grid
+      container
+      spacing={5}
+      sx={{
+        bgcolor: '#E9f5ff',
+      }}>
+      <Grid
+        container
+        sx={{
+          p: '2%',
+          width: '40%',
+          display: 'block',
+        }}>
+        <Autocomplete
+          options={topics}
+          getOptionLabel={(topic) => topic.name}
+          onInputChange={(event, newInputValue) => {
+            setSearchQuery(newInputValue);
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='Search Topics'
+              variant='filled'
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <Search
+                      sx={{
+                        color: 'gray.500',
+                        marginRight: '5px',
+                        marginBottom: '7px',
+                      }}
+                    />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+        />
+      </Grid>
+      <Grid container spacing={5}>
+        <AddTopicButton />
+        {topicListItems}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TopicList;
