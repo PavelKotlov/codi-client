1afecfac3441205dfa66c14e9f585deff109b15a
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:40:23 -0600

    add controllers

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,43 +1,49 @@
 import { Grid } from '@mui/material';
-import { useContext } from 'react';
 import React from 'react';
 import Title from '../components/dashboard/Title';
 import Stats from '../components/dashboard/Stats';
 import Buttons from '../components/dashboard/Buttons';
-import { topicContext } from '../providers/TopicProvider';
+import { useLocation } from 'react-router-dom';
+import NavMenu from '../components/controllers/menu';
+import CloseButton from '../components/controllers/closeButton';
 
 const Dashboard = () => {
-  const { name } = useContext(topicContext);
+  const location = useLocation();
+  const { name } = location.state;
 
   return (
-    <Grid
-      container
-      columnSpacing={3}
-      alignItems='center'
-      sx={{
-        height: '100vh',
-        p: '2%',
-        bgcolor: '#E9f5ff',
-      }}>
-      <Grid item xs={12} md={4}>
-        <Grid
-          container
-          alignItems='center'
-          justifyContent='center'
-          direction='column'
-          rowSpacing={7}>
-          <Grid item>
-            <Title name={name} />
-          </Grid>
-          <Grid item>
-            <Buttons />
+    <>
+      <NavMenu />
+      <CloseButton link='/topics' />
+      <Grid
+        container
+        columnSpacing={3}
+        alignItems='center'
+        sx={{
+          height: '100vh',
+          p: '2%',
+          bgcolor: '#E9f5ff',
+        }}>
+        <Grid item xs={12} md={5}>
+          <Grid
+            container
+            alignItems='center'
+            justifyContent='center'
+            direction='column'
+            rowSpacing={7}>
+            <Grid item>
+              <Title name={name} />
+            </Grid>
+            <Grid item>
+              <Buttons />
+            </Grid>
           </Grid>
         </Grid>
+        <Grid item xs={12} md={7} alignItems='center' sx={{ px: '10%' }}>
+          <Stats />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={8} alignItems='center' sx={{ px: '10%' }}>
-        <Stats />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
