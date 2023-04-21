1a18493bde987558b3e087c90ae005a2c460a95a
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:42:39 -0600

    topic list item component

---
+++ src/components/topics/TopicListItem.jsx
@@ -0,0 +1,21 @@
+import { Card, CardMedia, Grid } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TopicListItem = ({ name, image_url, id }) => {
+  return (
+    <Grid item xs={12} md={6} lg={3}>
+      <Link to={`/topics/${id}/dashboard`} state={{ name }} color='inherit'>
+        <Card elevation={4} sx={{ borderRadius: '15%' }}>
+          <CardMedia
+            sx={{ height: 0, paddingTop: '100%' }}
+            image={image_url}
+            title={name}
+          />
+        </Card>
+      </Link>
+    </Grid>
+  );
+};
+
+export default TopicListItem;
