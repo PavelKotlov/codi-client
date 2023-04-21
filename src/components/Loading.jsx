b365670a6727e0e149f4ad9e1268fdea3938c40e
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-21 00:37:49 -0600

    loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,19 @@
+import { CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+
+const Loading = ({}) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}>
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default Loading;
