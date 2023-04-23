b7e00e2ac95fbdd22a4c5a9d346cae41cad4977e
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-04-22 21:49:24 -0700

    refactor menu

--- src/components/controllers/menu.jsx
+++ src/components/controllers/menu.jsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import { Avatar, ListItemIcon, Typography } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Avatar, ListItemIcon, Typography } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const ITEM_HEIGHT = 48;
 
 export default function NavMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -19,22 +20,28 @@ export default function NavMenu() {
     setAnchorEl(null);
   };
 
+  const handleSettings = () => {
+    setAnchorEl(null);
+
+  };
+
   return (
     <div>
       <IconButton
-        sx={{ position: 'absolute', top: 0, right: 0, marginRight: 3 }}
-        aria-label='more'
-        id='long-button'
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup='true'
-        onClick={handleClick}>
-        <MoreHorizIcon fontSize='large' />
+        sx={{ position: "absolute", top: 0, right: 0, marginRight: 3 }}
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreHorizIcon fontSize="large" />
       </IconButton>
       <Menu
-        id='long-menu'
+        id="long-menu"
         MenuListProps={{
-          'aria-labelledby': 'long-button',
+          "aria-labelledby": "long-button",
         }}
         anchorEl={anchorEl}
         open={open}
@@ -42,29 +49,30 @@ export default function NavMenu() {
         PaperProps={{
           style: {
             maxHeight: ITEM_HEIGHT * 4.5,
-            width: '20ch',
+            width: "20ch",
           },
-        }}>
+        }}
+      >
         <MenuItem onClick={handleClose}>
           <Avatar />
-          <Typography variant='body2' color='text.secondary' p={1}>
+          <Typography variant="body2" color="text.secondary" p={1}>
             Sara Miller
           </Typography>
         </MenuItem>
-        <MenuItem key='setting' onClick={handleClose}>
+        <MenuItem key="setting" onClick={handleSettings}>
           <ListItemIcon>
-            <SettingsIcon fontSize='small' />
+            <SettingsIcon fontSize="small" />
           </ListItemIcon>
-          <Typography variant='body2' color='text.secondary'>
+          <Typography variant="body2" color="text.secondary">
             Settings
           </Typography>
         </MenuItem>
 
-        <MenuItem key='logout' onClick={handleClose}>
+        <MenuItem key="logout" onClick={handleClose}>
           <ListItemIcon>
-            <ExitToAppIcon fontSize='small' />
+            <ExitToAppIcon fontSize="small" />
           </ListItemIcon>
-          <Typography variant='body2' color='text.secondary'>
+          <Typography variant="body2" color="text.secondary">
             Logout
           </Typography>
         </MenuItem>
