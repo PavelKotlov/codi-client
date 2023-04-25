75a65366b9934c82d1e93b0530c4aef97ae1842d
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-25 16:55:13 -0600

    move get topics to user context

--- src/pages/topics.js
+++ src/pages/topics.js
@@ -1,54 +1,28 @@
-import React, { useContext, useEffect, useState } from "react";
-import axios from "axios";
-import TopicList from "../components/topics/TopicList";
-import Loading from "../components/Loading";
-import NavMenu from "../components/controllers/menu";
-import { Box } from "@mui/material";
-import CloseButton from "../components/controllers/CloseButton";
-import { UserContext } from "../providers/UserProvider";
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import TopicList from '../components/topics/TopicList';
+import Loading from '../components/Loading';
+import NavMenu from '../components/controllers/menu';
+import { Box } from '@mui/material';
+import CloseButton from '../components/controllers/CloseButton';
+import { UserContext } from '../providers/UserProvider';
 
 const Topics = () => {
-  const { token } = useContext(UserContext);
-  const [state, setState] = useState({
-    loading: true,
-    topics: [],
-  });
-
-  console.log("Token in topics", token);
-  useEffect(() => {
-    axios
-      .get(`/api/topics`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        setState((prev) => ({
-          ...prev,
-          topics: res.data,
-          loading: false,
-        }));
-      });
-  }, []);
+  const { loading } = useContext(UserContext);
 
   return (
     <Box
-      height="100vh"
-      bgcolor="background.light"
+      height='100vh'
+      bgcolor='background.light'
       sx={{
         backgroundImage: `url(${
-          process.env.PUBLIC_URL + "/assets/images/light/topics-bg.png"
+          process.env.PUBLIC_URL + '/assets/images/light/topics-bg.png'
         })`,
-        backgroundSize: "cover",
-      }}
-    >
-      <CloseButton link={"/"} isDefault={true} />
+        backgroundSize: 'cover',
+      }}>
+      <CloseButton link={'/'} isDefault={true} />
       <NavMenu showSettings={false} />
-      {state.loading ? (
-        <Loading />
-      ) : (
-        <TopicList state={state} setState={setState} />
-      )}
+      {loading ? <Loading /> : <TopicList />}
     </Box>
   );
 };
