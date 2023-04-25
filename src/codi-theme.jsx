8173975051c6ac09f1a876ec2fa0dca41249a4dd
PavelKotlov <pavelkotlov@gmail.com>
2023-04-25 01:54:26 -0700

    Setup Codi Theme, add custom color scheme

--- src/codi-theme.jsx
+++ src/codi-theme.jsx
@@ -1,5 +1,41 @@
 import { createTheme } from "@mui/material";
 
-const codiTheme = createTheme({});
+const codiTheme = createTheme({
+  palette: {
+    secondary: {
+      main: "#27E0C0",
+    },
+    info: {
+      main: "#7878EA",
+    },
+    success: {
+      main: "#D84177",
+      light: "#F4B8D6",
+    },
+    background: {
+      light: "#F3FAFF",
+      default: "#E9F5FF",
+    },
+    primaryCodi: {
+      main: "#E9F5FF",
+      mainHover: "#C0DBED",
+      light: "#F3FAFF",
+      medium: "#A8C4E5",
+      dark: "#271D30",
+    },
+    secondaryCodi: {
+      main: "#7878EA",
+      mainHover: "#B2B5F4",
+    },
+    accentsCodi: {
+      yellow: "#FFD22D",
+      yellowHover: "#FFAD08",
+      pink: "#D84177",
+      pinkHover: "#F4B8D6",
+      green: "#27E0C0",
+      greenHover: "#AAE8DC",
+    },
+  },
+});
 
 export default codiTheme;
