b49e4c4b1a77569d74bbfd805b1319c164884f36
PavelKotlov <pavelkotlov@gmail.com>
2023-04-25 22:22:25 -0700

    Refactor index

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         domain={process.env.REACT_APP_AUTH0_DOMAIN}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
         authorizationParams={{
-          redirect_uri: "https://codi-app.netlify.app/topics",
+          redirect_uri: "https://codi-app.netlify.app/",
           scope: "profile",
           audience: process.env.REACT_APP_AUTH0_AUDIANCE,
         }}
