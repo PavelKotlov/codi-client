8aa078f02beecd546d13369da5272c07b437f97d
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-04-27 14:52:12 -0600

    remove  fake data

--- src/components/dashboard/BarChart.jsx
+++ src/components/dashboard/BarChart.jsx
@@ -1,19 +1,16 @@
-import { Card, IconButton, Stack } from "@mui/material";
-import React from "react";
-import ReactEcharts from "echarts-for-react";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import { useContext } from "react";
-import { topicContext } from "../../providers/TopicProvider";
+import { Card, IconButton, Stack } from '@mui/material';
+import React from 'react';
+import ReactEcharts from 'echarts-for-react';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { useContext } from 'react';
+import { topicContext } from '../../providers/TopicProvider';
 
 const BarChart = ({ onClick }) => {
   const { ease } = useContext(topicContext);
 
   let easeFactors = ease.map((e) => e.ease_factor);
   let counts = ease.map((e) => e.count);
-
-  easeFactors = [1.8, 2.3, 2.4, 2.5, 2.65, 2.8];
-  counts = [1, 3, 8, 20, 4, 2];
 
   const option = {
     grid: {
@@ -23,8 +20,8 @@ const BarChart = ({ onClick }) => {
       height: 200,
     },
     xAxis: {
-      name: "Ease",
-      type: "category",
+      name: 'Ease',
+      type: 'category',
       data: easeFactors,
       axisTick: {
         show: false,
@@ -34,8 +31,8 @@ const BarChart = ({ onClick }) => {
       },
     },
     yAxis: {
-      name: "Cards",
-      type: "value",
+      name: 'Cards',
+      type: 'value',
       show: true,
       splitLine: {
         show: false,
@@ -50,33 +47,32 @@ const BarChart = ({ onClick }) => {
     series: [
       {
         data: counts,
-        type: "bar",
-        barWidth: "40%",
-        color: "#271D30",
+        type: 'bar',
+        barWidth: '40%',
+        color: '#271D30',
       },
     ],
     title: {
-      text: "Ease of Comprehension",
-      left: "center",
+      text: 'Ease of Comprehension',
+      left: 'center',
     },
   };
 
   return (
     <Card
       sx={{
-        bgcolor: "#fff",
-        display: "flex",
-        borderRadius: "25px",
-        width: "90%",
-        margin: "0 auto",
-        boxShadow: "-10px 10px 10px rgba(62, 32, 102, .5)",
-      }}
-    >
-      <Stack direction={"row"}>
+        bgcolor: '#fff',
+        display: 'flex',
+        borderRadius: '25px',
+        width: '90%',
+        margin: '0 auto',
+        boxShadow: '-10px 10px 10px rgba(62, 32, 102, .5)',
+      }}>
+      <Stack direction={'row'}>
         <IconButton sx={{ borderRadius: 0 }} onClick={onClick}>
           <ArrowBackIosNewIcon />
         </IconButton>
-        <div style={{ width: "110%", height: 275 }}>
+        <div style={{ width: '110%', height: 275 }}>
           <ReactEcharts option={option} />
         </div>
         <IconButton sx={{ borderRadius: 0 }} onClick={onClick}>
--- src/components/dashboard/Gauge.jsx
+++ src/components/dashboard/Gauge.jsx
@@ -1,45 +1,43 @@
-import { Card, IconButton, Stack } from "@mui/material";
-import React from "react";
-import ReactEcharts from "echarts-for-react";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import { useContext } from "react";
-import { topicContext } from "../../providers/TopicProvider";
+import { Card, IconButton, Stack } from '@mui/material';
+import React from 'react';
+import ReactEcharts from 'echarts-for-react';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { useContext } from 'react';
+import { topicContext } from '../../providers/TopicProvider';
 
 const Gauge = ({ onClick }) => {
   let { maturityPercentage, topic } = useContext(topicContext);
 
-  maturityPercentage = 30;
-
   const option = {
     series: [
       {
-        type: "gauge",
+        type: 'gauge',
         startAngle: 180,
         endAngle: 0,
         responsive: true,
         maintainAspectRatio: false,
-        center: ["50%", "60%"],
+        center: ['50%', '60%'],
         axisLine: {
           lineStyle: {
             width: 30,
             color: [
-              [0.5, "#A8C4E5"],
-              [0.75, "#7878EA"],
-              [1, "#271D30"],
+              [0.5, '#A8C4E5'],
+              [0.75, '#7878EA'],
+              [1, '#271D30'],
             ],
           },
         },
         pointer: {
           itemStyle: {
-            color: "inherit",
+            color: 'inherit',
           },
         },
         axisTick: {
           distance: -30,
           length: 8,
           lineStyle: {
-            color: "#fff",
+            color: '#fff',
             width: 2,
           },
         },
@@ -47,20 +45,20 @@ const Gauge = ({ onClick }) => {
           distance: -30,
           length: 30,
           lineStyle: {
-            color: "#fff",
+            color: '#fff',
             width: 4,
           },
         },
         axisLabel: {
           show: false,
-          color: "inherit",
+          color: 'inherit',
           distance: 40,
           fontSize: 12,
         },
         detail: {
           valueAnimation: true,
-          formatter: "Topic Comprehension",
-          color: "#271D30",
+          formatter: 'Topic Comprehension',
+          color: '#271D30',
           fontSize: 18,
         },
         data: [
@@ -75,19 +73,18 @@ const Gauge = ({ onClick }) => {
   return (
     <Card
       sx={{
-        bgcolor: "#fff",
-        display: "flex",
-        borderRadius: "25px",
-        width: "90%",
-        margin: "0 auto",
-        boxShadow: "-10px 10px 10px rgba(62, 32, 102, .5)",
-      }}
-    >
-      <Stack direction={"row"}>
+        bgcolor: '#fff',
+        display: 'flex',
+        borderRadius: '25px',
+        width: '90%',
+        margin: '0 auto',
+        boxShadow: '-10px 10px 10px rgba(62, 32, 102, .5)',
+      }}>
+      <Stack direction={'row'}>
         <IconButton sx={{ borderRadius: 0 }} onClick={onClick}>
           <ArrowBackIosNewIcon />
         </IconButton>
-        <div style={{ width: "100%", height: 275 }}>
+        <div style={{ width: '100%', height: 275 }}>
           <ReactEcharts option={option} />
         </div>
         <IconButton sx={{ borderRadius: 0 }} onClick={onClick}>
